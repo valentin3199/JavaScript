1120a1d4d87768212aa575276f7f435131e1b55b
Valentin Nicolae <nicolae9931@yahoo.com>
2019-08-28 23:26:24 +0100

    Update for Variables and Data type

---
+++ src/index.js
@@ -0,0 +1,50 @@
+
+// var me = 1;
+// console.log(typeof me); //You find which type it is
+
+// //fractional numbers
+// var y = 2.4;
+
+// //5.3 * 10^8
+// var z = 5.3e8;
+// console.log(z);
+
+// //arithmetic operation
+// var a = 4 + 9;
+// console.log(a);
+
+// a = 3 * 8;
+// console.log(a);
+
+// a = y - z;
+// console.log(a) ;
+
+
+  //CONSTANTS, you can't change
+// const firstname = 'VALI';
+
+      // //declare a VARIABLE and in initialize
+// var livesLeft = 3;
+// console.log(livesLeft);
+      // //re-assign a value to our VARIABLE
+// livesLeft = 2;
+// console.log(livesLeft);
+
+  //declare a veriable
+// var val;
+// console.log(val);
+
+    // print something in the console
+// console.log('My first poject with JavaScript!');
+
+    //show an alert message saying H1
+// window.alert('h1');
+
+
+
+
+
+
+
+// var change = 'JS'
+// document.querySelector('h1').innerHTML = change;
